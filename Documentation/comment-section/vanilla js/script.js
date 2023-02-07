383bedc46fbd9f6cfd38b89b1a9ef8c548a3a8d6
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-02-07 09:44:10 +0300

    Revised script file

---
+++ Documentation/comment-section/vanilla js/script.js
@@ -0,0 +1,9 @@
+console.log("HDcomments init");
+
+const HDC_EL {
+    submit: document.getElementById(""),
+        comment: document.getElementById(""),
+            email: document.getElementById(""),
+                user_name: document.getElementById(""),
+                    reactions: document.getElementsByClassName("")
+}
