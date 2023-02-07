beae62da200d16153fa39e097ff329580cec932d
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-02-07 10:41:55 +0300

    script updated

--- Documentation/comment-section/vanilla js/script.js
+++ Documentation/comment-section/vanilla js/script.js
@@ -1,9 +1,31 @@
 console.log("HDcomments init");
 
-const HDC_EL {
-    submit: document.getElementById(""),
-        comment: document.getElementById(""),
-            email: document.getElementById(""),
-                user_name: document.getElementById(""),
-                    reactions: document.getElementsByClassName("")
+const HDC_EL = {
+    submit: document.getElementById("hdc_submit"),
+    comment: document.getElementById("hdc_comment_input"),
+    email: document.getElementById("hdc_email_input"),
+    user_name: document.getElementById("hdc_name_input"),
+    reactions: document.getElementsByClassName("hdc_reaction")
+};
+
+function hdc_canSubmit() {
+    //Check the required fields
+    let comment = HDC_EL.comment.nodeValue.trim();
+    let email = HDC_EL.email.nodeValue.trim();
+    let name = HDC_EL.user_name.nodeValue.trim();
+    if (comment.length > 4 && email.length > 4 && name.length > 4) {
+        HDC_EL.submit.classList.add("hdc_submit_enabled");
+        HDC_EL.submit.disabled = false;
+        console.log("good to go");
+    } else {
+        HDC_EL.submit.classList.remove("hdc_submit_enabled");
+    }
 }
+
+function hdc_set_event_listeners() {
+    HDC_EL.comment.addEventListener("keyup", "hdc_canSubmit");
+    HDC_EL.email.addEventListener("keyup", "hdc_canSubmit");
+    HDC_EL.user_name.addEventListener("keyup", "hdc_canSubmit");
+}
+
+hdc_set_event_listeners();
