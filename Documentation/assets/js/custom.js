1dba25e115efd9d0068cb2e7f7ed93e268a0088f
muhindablasio@gmail.com <muhindablasio@gmail.com>
2023-03-09 20:24:46 +0300

    Files updated

--- Documentation/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-const imageSelect = document.querySelector('.image');
-const pseudoElement = window.getComputedStyle(imageSelect, '::after');
-
-pseudoElement.addEventListener('click', function () {
-    window.location.href = "https://www.google.com";
-});
